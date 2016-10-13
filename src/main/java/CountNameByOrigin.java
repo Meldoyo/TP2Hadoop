32e97929f535e59484cdc0ff89ef93be4fa9b26b
Paul-Adrien Cordonnier <pa.cordonnier@gmail.com>
2016-10-13 15:47:31 +0200

    Count Name by origin, no combiner

--- src/main/java/CountNameByOrigin.java
+++ src/main/java/CountNameByOrigin.java
@@ -1,29 +1,30 @@
 import java.io.IOException;
 
-import java.io.IOException;
 import java.util.*;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
 
-public class WordCount {
-    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+public class CountNameByOrigin {
+    private static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
         public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             String line = value.toString();
-            StringTokenizer tokenizer = new StringTokenizer(line);
-            while (tokenizer.hasMoreTokens()) {
-                word.set(tokenizer.nextToken());
+            String[] lineSplit = line.split(";");
+            String[] originSplit = lineSplit[2].split(",");
+            for (String s : originSplit) {
+                if (s.equals("")) {
+                    s = "?";
+                }
+                word.set(s.trim());
                 output.collect(word, one);
             }
         }
     }
 
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+    private static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
         public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             int sum = 0;
             while (values.hasNext()) {
@@ -34,8 +35,8 @@ public class WordCount {
     }
 
     public static void main(String[] args) throws Exception {
-        JobConf conf = new JobConf(WordCount.class);
-        conf.setJobName("wordCount");
+        JobConf conf = new JobConf(CountNameByOrigin.class);
+        conf.setJobName("originCount");
         conf.setOutputKeyClass(Text.class);
         conf.setOutputValueClass(IntWritable.class);
 
